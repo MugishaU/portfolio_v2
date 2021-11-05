176bb4269f25fab2e1b9c1fc3a9eb769e2b47b8c
MugishaU <muwiragiye123@gmail.com>
2021-11-05 00:08:57 +0000

    create under construction page

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,64 +12,36 @@ const Home: NextPage = () => {
 					name="description"
 					content="Mugisha Uwiragiye's Developer Portfolio Highlighting Projects and Key Skills"
 				/>
-				<link rel="icon" href="/memoji.png" />
+				<link rel="icon" href="/memoji-laptop.png" />
+				<meta name="theme-color" content="#0A192E" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="true"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+					rel="stylesheet"
+				/>
 			</Head>
 
 			<main className={styles.main}>
-				<h1 className={styles.title}>
-					Welcome to <a href="https://nextjs.org">Next.js!</a>
-				</h1>
+				<h1 className={styles.title}>Mugisha Uwiragiye</h1>
+				<h2>This site is under construction, check back soon!</h2>
 
-				<h2>Staging</h2>
-
-				<p className={styles.description}>
-					Get started by editing{" "}
-					<code className={styles.code}>pages/index.tsx</code>
-				</p>
-
-				<div className={styles.grid}>
-					<a href="https://nextjs.org/docs" className={styles.card}>
-						<h2>Documentation &rarr;</h2>
-						<p>Find in-depth information about Next.js features and API.</p>
-					</a>
-
-					<a href="https://nextjs.org/learn" className={styles.card}>
-						<h2>Learn &rarr;</h2>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
-					</a>
-
-					<a
-						href="https://github.com/vercel/next.js/tree/master/examples"
-						className={styles.card}
-					>
-						<h2>Examples &rarr;</h2>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
-
-					<a
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className={styles.card}
-					>
-						<h2>Deploy &rarr;</h2>
-						<p>
-							Instantly deploy your Next.js site to a public URL with Vercel.
-						</p>
-					</a>
-				</div>
-			</main>
-
-			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{" "}
+				<footer>
 					<span className={styles.logo}>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+						<Image
+							src="/memoji-hi.png"
+							alt="Memoji of Mugisha"
+							width={100}
+							height={100}
+							priority
+						/>
 					</span>
-				</a>
-			</footer>
+				</footer>
+			</main>
 		</div>
 	);
 };
