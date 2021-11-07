b95b7a54348e9894ae175a72ffe8c244370cba0d
MugishaU <muwiragiye123@gmail.com>
2021-11-07 23:03:41 +0000

    sw scope

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const runtimeCaching = require("next-pwa/cache");
 module.exports = withPWA({
 	reactStrictMode: true,
 	pwa: {
+		dest: "public",
 		runtimeCaching,
-		buildExcludes: [/middleware-manifest\.json$/],
+		buildExcludes: [/middleware-manifest.json$/],
 	},
 });
