2f9a18b21a4c964bbc92923b7afffa16e58b1629
MugishaU <muwiragiye123@gmail.com>
2021-11-07 17:31:45 +0000

    Create head tag with SEO friendly features

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,47 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta charSet="utf-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta
+					name="viewport"
+					content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+				/>
+				<meta
+					name="description"
+					content="Mugisha Uwiragiye's developer portfolio highlighting his projects and skills. Find out more about him, and how he builds secure, scalable solutions."
+				/>
+				<meta
+					name="keywords"
+					content="coding,developer,engineering,mugisha,programming,software,uwiragiye"
+				/>
+				<meta name="theme-color" content="#0A192E" />
+
+				{/* <link rel="manifest" href="/manifest.json" /> */}
+				<link href="/icons/memoji-laptop.png" rel="icon" type="image/png" />
+				<link rel="apple-touch-icon" href="/icons/memoji-laptop.png"></link>
+				<link rel="canonical" href="https://www.mugisha.io/" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="true"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+					rel="stylesheet"
+				/>
+
+				<title>Mugisha Uwiragiye</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,17 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
 	return (
 		<div className={styles.container}>
-			<Head>
-				<title>Mugisha Uwiragiye</title>
-				<meta
-					name="description"
-					content="Mugisha Uwiragiye's Developer Portfolio Highlighting Projects and Key Skills"
-				/>
-				<link rel="icon" href="/memoji-laptop.png" />
-				<meta name="theme-color" content="#CCD7F5" />
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossOrigin="true"
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
-					rel="stylesheet"
-				/>
-			</Head>
-
 			<main className={styles.main}>
 				<h1 className={styles.title}>Mugisha Uwiragiye</h1>
 				<h2>This site is under construction, check back soon!</h2>
-
 				<footer>
 					<span className={styles.logo}>
 						<Image
-							src="/memoji-hi.png"
+							src="/icons/memoji-hi.png"
 							alt="Memoji of Mugisha"
 							width={100}
 							height={100}
