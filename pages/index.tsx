71b1256f2b42ad453328aa53adfdd10a23b07219
MugishaU <mugisha@lapse.com>
2024-08-26 20:45:44 +0100

    add contact information

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,32 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-	return (
-		<div className={styles.container}>
-			<main className={styles.main}>
-				<h1 className={styles.title}>Mugisha Uwiragiye</h1>
-				<h2>This site is under construction, check back soon!</h2>
-				<footer>
-					<span className={styles.logo}>
-						<Image
-							// src="/images/memoji-hi.png"
-							src="/icons/icon-512x512.png"
-							alt="Memoji of Mugisha"
-							width={100}
-							height={100}
-							priority
-						/>
-					</span>
-				</footer>
-			</main>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Mugisha Uwiragiye</h1>
+        <h2>
+          To get in touch with an inquiry, email{" "}
+          <a className={styles.email} href="mailto:me@mugisha.io?subject=Inquiry">
+            me@mugisha.io
+          </a>
+        </h2>
+
+        <footer>
+          <span className={styles.logo}>
+            <Image
+              src="/icons/icon-512x512.png"
+              alt="Memoji of Mugisha"
+              width={100}
+              height={100}
+              priority
+            />
+          </span>
+        </footer>
+        <h4>This site is under construction, check back soon!</h4>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
